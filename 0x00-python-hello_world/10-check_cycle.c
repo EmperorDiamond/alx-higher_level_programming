0567de7a955ce126ae3f707522d4a27c3f47f93b
Emmanuel Olushola Ajayi <diamondolushola@gmail.com>
2022-10-29 22:17:11 +0100

    Python_Tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if linked list has a cycle
- * @list: node in linked list
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * check_cycle - checks to see if a list is in an endless loop or cycle
+ * @list: the list to check
+ * Return: 0 if no cycle is detected, 1 if there is a cycle
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *i = list;
+	listint_t *doub = list;
+	listint_t *reg = list;
 
-	if (!list)
+	if (list == NULL)
 		return (0);
 
-	list = list->next;
-	while (list)
+	while (doub && doub->next)
 	{
-		if (i == list)
+		reg = reg->next;
+		doub = doub->next->next;
+
+		if (reg == doub)
 			return (1);
-		if (!(list->next) || !(list->next->next))
-			return (0);
-		i = i->next;
-		list = list->next->next;
 	}
 	return (0);
 }
